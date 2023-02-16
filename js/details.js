61c7b7b3a9f000770b3d4a056fded886b54cc3fa
W-Coulthard <96620785+W-Coulthard@users.noreply.github.com>
2023-02-16 13:16:16 +0000

    trailers fixed

--- js/details.js
+++ js/details.js
@@ -29,7 +29,7 @@ const TRAILER_API = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_ke
 
 function showMovieDetails(data) {
   // Get the movie details from the data returned from the API
-  const { poster_path, title, vote_average, overview, cast = [], release_date, tagline, genres, backdrop_path, runtime, video } = data
+  const { poster_path, title, vote_average, overview, cast = [], release_date, tagline, genres, backdrop_path, runtime } = data
   
   let castList = ''
   cast.forEach(actor => {
@@ -49,23 +49,69 @@ function showMovieDetails(data) {
       <div class="runtime">Runtime: ${runtime}min</div>
       <div class="genres">Genres: 
       ${genres.map(genre => `<p>${genre.name}</p>`).join(', ')}</div>
-      <div class="video">${video}</div>
       <div class="tagline">"${tagline}"</div>
-      <button id="trailer-button">Watch Trailer</button>
-<div id="trailer-container" style="display: block;">
-    <video id="trailer" controls>
-      <source src="">
-      Your browser does not support the video tag.
-    </video>
-      <p>Cast:</p>
+      <div class="cast-list">Cast: </div>
       
-
     </div>
     </div>`
-    
-    
+  
+  // Create a "Watch Trailer" button
+const trailerButton = document.createElement('button');
+trailerButton.innerHTML = 'Watch Trailer';
+trailerButton.classList.add('trailer-button');
+
+const movieInfo = document.querySelector('.movie-info');
+
+if (movieInfo) {
+  movieInfo.appendChild(trailerButton);
+} else {
+  console.error('Movie info element not found.');
 }
 
+// Function to get the trailer URL from the API
+async function getTrailerUrl() {
+  const trailersApi = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
+  const res = await fetch(trailersApi);
+  const data = await res.json();
+
+  // Get the first trailer URL
+  const trailerUrl = `https://www.youtube.com/watch?v=${data.results[0].key}`;
+  console.log(trailerUrl);
+
+  // Do something with the trailer URL, like opening it in a new window
+  window.open(trailerUrl);
+  
+}
+
+// Add event listener to the "Watch Trailer" button
+trailerButton.addEventListener('click', getTrailerUrl);
+
+/*Cast API*/
+fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`)
+  .then(response => response.json())
+  .then(data => {
+  // console.log(data);
+  // do something with the cast data
+    const cast = data.cast;
+
+const castList = document.createElement('div');
+castList.classList.add('cast-list');
+
+cast.forEach(actor => {
+  const actorEl = document.createElement('p');
+  actorEl.classList.add('actor');
+  actorEl.innerText = actor.name;
+  castList.appendChild(actorEl);
+});
+
+const movieInfoEl = movieContainer.querySelector('.movie-info');
+movieInfoEl.appendChild(castList);
+
+  })
+  .catch(error => {
+    console.error('Error fetching cast data:', error);
+  });
+}
 
 async function getMovieDetails() {
   const res = await fetch(DETAILS_API)
@@ -76,9 +122,7 @@ async function getMovieDetails() {
 
 // Call the function to get and show the movie details
 getMovieDetails();
-
 //END
-
 
 const getMovies = (url) => {
   fetch(url)  
@@ -111,10 +155,11 @@ const showMovies = (movies) => {
 }
 
 /*Cast API*/
+/*
 fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+   // console.log(data);
     // do something with the cast data
     const cast = data.cast;
 
@@ -123,6 +168,7 @@ castList.classList.add('cast-list');
 
 cast.forEach(actor => {
   const actorEl = document.createElement('p');
+  actorEl.classList.add('actor');
   actorEl.innerText = actor.name;
   castList.appendChild(actorEl);
 });
@@ -135,8 +181,9 @@ movieInfoEl.appendChild(castList);
     console.error('Error fetching cast data:', error);
   });
 
-
+*/
   /*search bar*/
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const searchTerm = search.value;
@@ -150,227 +197,7 @@ form.addEventListener('submit', (e) => {
 })
 console.log(DETAILS_API)
 
-//Trailer API
-
-//Trailer
 
 
-const trailerButton = document.createElement('trailer-button');
-trailerButton.innerHTML = 'Watch Trailer';
-trailerButton.classList.add('trailer-button');
-
-const movieInfo = document.querySelector('.movie-info');
-//movieInfo.appendChild(trailerButton);
-// Add an event listener to the trailer button
-trailerButton.addEventListener('click', () => {
-  getTrailerUrl();
-});
-// Function to get the trailer URL from the API
-async function getTrailerUrl() {
-  const trailersApi = TRAILER_API;
-  const res = await fetch(trailersApi);
-  const data = await res.json();
-  // Get the first trailer URL
-  const trailerUrl = data.results[0].key;
-  console.log(trailerUrl);
-  // Do something with the trailer URL, like opening it in a new window
-  window.open(TRAILER_API + trailerUrl);
-}
-window.onload = function() {
-  getTrailerUrl();
-
-  
-};
-
-
-
-
-
-/*
-//Trailer
-const express = require("express");
-const cors = require("cors");
-const app = express();
-
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-module.exports = app;
-
-const trailerButton = document.getElementById("trailer-button");
-const trailerContainer = document.getElementById("trailer-container");
-const trailer = document.getElementById("trailer");
-
-trailerButton.addEventListener("click", function() {
-  trailerContainer.style.display = "block";
-  trailer.src = "https://www.youtube.com/embed/9O1Iy9od7-A";
-});
-
-/*
-<button id="trailer-button">Watch Trailer</button>
-<div id="trailer-container" style="display: none;">
-    <video id="trailer" controls>
-      <source src="">
-      Your browser does not support the video tag.
-    </video>
-</div>
-*/
-/*
-ToDo:
-1. cast delay
-2. Add trailer
-3. css
-*/
-
-/*
-//Trailer
-
-const trailerButton = document.createElement('button');
-trailerButton.innerHTML = 'Watch Trailer';
-trailerButton.classList.add('trailer-button');
-
-const movieInfo = document.querySelector('.movie-info');
-movieInfo.appendChild(trailerButton);
-
-// Add an event listener to the trailer button
-trailerButton.addEventListener('click', () => {
-  // Call the function to get the trailer URL from the API
-  getTrailerUrl();
-});
-
-// Function to get the trailer URL from the API
-async function getTrailerUrl() {
-  const trailersApi = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
-  const res = await fetch(trailersApi);
-  const data = await res.json();
-
-  // Get the first trailer URL
-  const trailerUrl = data.results[0].key;
-  console.log(trailerUrl);
-
-  // Do something with the trailer URL, like opening it in a new window
-  window.open(`https://www.youtube.com/watch?v=${trailerUrl}`);
-}
-//Trailer
-
-const trailerButton = document.createElement('button');
-trailerButton.innerHTML = 'Watch Trailer';
-trailerButton.classList.add('trailer-button');
-
-const movieInfo = document.querySelector('.movie-info');
-movieInfo.appendChild(trailerButton);
-
-// Add an event listener to the trailer button
-trailerButton.addEventListener('click', () => {
-  // Call the function to get the trailer URL from the API
-  getTrailerUrl();
-});
-
-//Trailer
-
-const trailerButton = document.getElementById("trailer-button");
-const trailerContainer = document.getElementById("trailer-container");
-const trailer = document.getElementById("trailer");
-
-trailerButton.addEventListener("click", function() {
-  trailerContainer.style.display = "block";
-  trailer.src = `https://www.youtube.com/watch?v=${movieId}`;
-});
-
-//end
-
-async function getTrailerUrl(movieId) {
-  // Make a GET request to the API to retrieve the trailer URL
-  const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`);
-  const data = await response.json();
-  const trailerUrl = data.trailerUrl;
-
-  // Play the trailer using the retrieved URL
-  const videoPlayer = document.getElementById('videoPlayer');
-  videoPlayer.src = trailerUrl;
-  videoPlayer.play();
-}
-
-        <video id="videoPlayer" width="320" height="240" controls>
-            <source id="videoPlayer" src="" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-
-   //Trailer
-
-const trailerButton = document.createElement('button');
-trailerButton.innerHTML = 'Watch Trailer';
-trailerButton.classList.add('trailer-button');
-
-//const movieInfo = document.querySelector('.movie-info');
-movieInfo.appendChild(trailerButton);
-// Add an event listener to the trailer button
-trailerButton.addEventListener('click', () => {
-  getTrailerUrl();
-});
-// Function to get the trailer URL from the API
-async function getTrailerUrl() {
-  const trailersApi = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
-  const res = await fetch(trailersApi);
-  const data = await res.json();
-  // Get the first trailer URL
-  const trailerUrl = data.results[0].key;
-
-  // Do something with the trailer URL, like opening it in a new window
-  window.open(`https://www.youtube.com/watch?v=${trailerUrl}`, '_blank');
-}
-window.open(`https://www.youtube.com/watch?v=${trailers}`, '_blank');
-     
-//Trailer
-
-
-const watchTrailer = document.createElement('button');
-watchTrailer.innerHTML = 'Watch Trailer';
-watchTrailer.classList.add('watch-trailer');
-watchTrailer.addEventListener('click', () => {
-  const trailer = document.createElement('iframe');
-  trailer.src = `https://www.youtube.com/embed/${movieId}`;
-  trailer.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-  trailer.frameborder = "0";
-  trailer.width = "560";
-  trailer.height = "315";
-  movieContainer.appendChild(trailer);
-});
-
-
-const detailsSection = document.querySelector(".movie-info");
-detailsSection.appendChild(watchTrailer);
-      
-
-//Trailer
-
-const trailerButton = document.createElement('button');
-trailerButton.innerHTML = 'Watch Trailer';
-trailerButton.classList.add('trailer-button');
-
-//const movieInfo = document.querySelector('.movie-info');
-movieInfo.appendChild(trailerButton);
-// Add an event listener to the trailer button
-trailerButton.addEventListener('click', () => {
-  getTrailerUrl();
-});
-// Function to get the trailer URL from the API
-async function getTrailerUrl() {
-  const trailersApi = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
-  const res = await fetch(trailersApi);
-  const data = await res.json();
-  // Get the first trailer URL
-  const trailerUrl = data.results[0].key;
-
-  // Do something with the trailer URL, like opening it in a new window
-  window.open(`https://www.youtube.com/watch?v=${trailerUrl}`, '_blank');
-}
-window.
-
-https://www.youtube.com/watch?v=Q0CbN8sfihY
-*/ 
 
 
